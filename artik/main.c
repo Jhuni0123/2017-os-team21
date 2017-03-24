f039078402c652e3d55088da6fe6858bc271725d
dnsdhrj <dnsdhrj123@gmail.com>
2017-03-24 22:54:49 +0900

    Fix pretty printing

--- artik/main.c
+++ artik/main.c
@@ -15,23 +15,16 @@ int main()
 	int result = syscall(380, buf, &nr);
 	
 	// print syscall result
-	int i, prev_pid = 0, tab = 0;
-	int stack[20];
-	stack[0] = 0;
-	int* sp = stack;
+	int i, tab = 0;
+	int prev_pid = -1;
+	int prev_parent = (struct prinfo*)buf->parent_pid;
 	for(i = 0; i < result; i++) {
 		struct prinfo* p = (struct prinfo*)buf + i;
-		if(*sp != p->parent_pid) {
-			if(prev_pid == p->parent_pid) {
+		if(prev_parent != p->parent_pid)
+			if(prev_pid == p->parent_pid)
 				tab++;
-				sp++;
-				*sp = p->parent_pid;	
-			}
-			else {
+			else
 				tab--;
-				sp--;
-			}
-		}
 		int j;
 		char line[100];
 		line[0] = '\0';
@@ -40,7 +33,9 @@ int main()
 		strcat(line, "%s,%d,%ld,%d,%d,%d,%d\n");
 		printf(line, p->comm, p->pid, p->state,
 			p->parent_pid, p->first_child_pid, p->next_sibling_pid, p->uid);
-		prev_pid = p->parent_pid;
+		
+		prev_parent = p->parent_pid;
+		prev_pid = p->pid;
 	}
 
 	//return test(); //uncomment this to run test
