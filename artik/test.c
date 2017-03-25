2fe1f8e08b2b4a2256e897d8925709ce9fe0f713
dnsdhrj <dnsdhrj123@gmail.com>
2017-03-25 14:37:30 +0900

    fix test

--- artik/test.c
+++ artik/test.c
@@ -3,17 +3,18 @@
 #include <sys/syscall.h>
 #include <errno.h>
 #include <stdlib.h>
+#include "prinfo.h"
 
 #define FAIL(str) {printf("FAIL: "str"\n"); all_success = 0;}
 int test()
 {
 	int all_success = 1;
-
-	void* buf = malloc(10000);
-	int nr = 200;
 	
-	void* small_buf = malloc(100);
+	int nr = 500;
+	void* buf = malloc(sizeof(struct prinfo) * nr);
+	
 	int small_nr = 2;
+	void* small_buf = malloc(sizeof(struct prinfo) * small_nr);
 	int negative_nr = -1;
 	int zero_nr = 0;
 	
@@ -23,8 +24,10 @@ int test()
 	
 	if(syscall(380, buf, &nr) < 0)
 		FAIL("valid function call");
-	if(syscall(380, buf) >= 0 || syscall(380, buf, &nr, NULL) >= 0)
-		FAIL("invalid parameter number");
+	if(syscall(380, buf) >= 0)
+		FAIL("invalid parameter number, small chance to give valid nr");
+	if(syscall(380, buf, &nr, buf) < 0)
+		FAIL("valid function call with 3 parameters");
 	if(syscall(380, NULL, &nr) >= 0 || errno != EINVAL)
 		FAIL("null buf");
 	if(syscall(380, (unsigned)-10000, &nr) >= 0 || errno != EFAULT)
@@ -44,7 +47,7 @@ int test()
 		FAIL("valid function call with nr=0");	
 	
 	syscall(380, buf, &small_nr);
-	if(*((int*)buf + 25) != 0)
+	if(*(int*)((struct prinfo*)buf + small_nr + 1) != 0)
 		FAIL("copied more than nr");
 	
 	if(all_success)
