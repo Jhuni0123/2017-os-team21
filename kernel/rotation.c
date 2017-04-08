4bd4cf5d6fe2ea58cb3523da710354c8c0c7fd51
dajung <ss01363@naver.com>
2017-04-09 01:52:24 +0900

    finish primitive implementation of rotation.c functions do_rotlock_write, do_rotunlock_read, do_rotunlock_write are implemented. compiles well except for syscall functions. todo's : locks, sleep, wake, test, debug

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -31,7 +31,7 @@ int is_locked_write = 0;
 int is_waiting_write = 0;
 struct range_desc *w_write = NULL;
 
-int A()
+int A() /* always use with lock x */
 {
 	/* initialize variables */
 	is_locked_write = 0;
@@ -109,8 +109,16 @@ int A()
 
 int do_rotlock_read(int degree, int range)
 {
+	/* check if degree and range is correct */
+	if(degree < 0 || degree > 360){
+		printk("DEBUG: error: degree out of range\n");
+		return -1;
+	}
+	if(range < 0 || range > 360){
+		printk("DEBUG: error: range out of range\n");
+		return -1;
+	}
 	// todo: error checking
-
 	/* make a new range_desc to hold lock info */
 	struct range_desc* newitem =
 		(struct range_desc*) kmalloc(sizeof(struct range_desc), GFP_KERNEL);
@@ -129,12 +137,13 @@ int do_rotlock_read(int degree, int range)
 	// todo: set lock x (reason: access vars is_locked_write, is_waiting_write)
 	if(range_in_rotation(newitem)&&(!is_locked_write)&&(!is_waiting_write)){
       	list_add_tail(&newitem->node, &assigned_reads.node);
-      	return 0;
+      	// todo: unset lock x
+		return 0;
 	}
-	// todo: unset lock x
 
 	/* If it cannot be assigned, add it to wait and  make thread sleep */
-	list_add_tail(&newitem->node, &waiting_locks.node);
+	list_add_tail(&newitem->node, &waiting_locks.node);	
+	// todo: unset lock x
 	// todo: make it sleep
 
 	return 0;
@@ -143,6 +152,96 @@ int do_rotlock_read(int degree, int range)
 
 int do_rotlock_write(int degree, int range)
 {
+	// todo: error checking
+
+	/* make a new range_desc to hold lock info */
+	struct range_desc* newitem =
+		(struct range_desc*) kmalloc(sizeof(struct range_desc), GFP_KERNEL);
+	if(newitem == NULL)
+		return -ENOMEM;
+
+	newitem->type = WRITE_LOCK_FLAG;
+	newitem->degree = degree;
+	newitem->range = range;
+	newitem->tid = task_pid_vnr(current);
+	INIT_LIST_HEAD(&newitem->node);
+
+	/* Make it sleep, put it into waiting list.
+	 * If device lock is in newitem's range, call A */
+	// todo: make it sleep
+	// todo: set lock x 
+	list_add_tail(&newitem->node, &waiting_locks.node);
+	if(range_in_rotation(newitem))
+      	A();	
+	// todo: unset lock x
+
+	return 0;
+	
+}
+
+int do_rotunlock_read(int degree, int range)
+{
+	/* find that exact lock */
+	int is_there = 0;
+	struct range_desc *curr = NULL;
+	list_for_each_entry(curr, &assigned_reads.node, node){
+		if((curr->type == READ_LOCK_FLAG)
+				&&(curr->degree == degree)
+				&&(curr->range == range)
+				&&(curr->tid == task_pid_vnr(current))){
+			is_there = 1;
+			break;
+		}
+	}
+	if(!is_there){
+		printk("DEBUG: error: no match for rotunlock_read\n");
+		return -1;
+	}
+	// todo: error check
+	
+	/* delete it from list.
+	 * If device lock is in curr's range, call A */
+	// todo: set lock x 
+	list_del(&curr->node);
+	if(range_in_rotation(curr))
+      	A();	
+	// todo: unset lock x
+
+	return 0;
+
+}
+
+int do_rotunlock_write(int degree, int range)
+{
+	/* find that exact lock */
+	int is_there = 0;
+	struct range_desc *curr = NULL;
+	list_for_each_entry(curr, &assigned_writes.node, node){
+		if((curr->type == WRITE_LOCK_FLAG)
+				&&(curr->degree == degree)
+				&&(curr->range == range)
+				&&(curr->tid == task_pid_vnr(current))){
+			is_there = 1;
+			break;
+		}
+	}
+	if(!is_there){
+		printk("DEBUG: error: no match for rotunlock_write\n");
+		return -1;
+	}
+	// todo: error check
+	
+	/* delete it from list.
+	 * If device lock is in curr's range, call A */
+	// todo: set lock x 
+	list_del(&curr->node);
+	if(range_in_rotation(curr))
+      	A();	
+	// todo: unset lock x
+
+	return 0;
+
+
 }
 
 int do_set_rotation(int degree)
@@ -169,8 +268,7 @@ SYSCALL_DEFINE2(rotlock_read, int, degree, int, range)
 
 SYSCALL_DEFINE2(rotlock_write, int, degree, int, range)
 {
-	return 0;
-	//return do_rotlock_write(degree, range);
+	return do_rotlock_write(degree, range);
 }
 
 SYSCALL_DEFINE2(rotunlock_read, int, degree, int, range)
