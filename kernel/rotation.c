58374ff644d2766f91d49926e40c9879715548cb
Jhuni0123 <whdgnsdl887@gmail.com>
2017-04-11 17:10:23 +0900

    Fix errors & Refactor
    Initialize range_desc
    Fix lock constraint
    Use enum for r/w flag

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -10,43 +10,27 @@
 int device_rot = INITIAL_ROT;
 int range_desc_init = 0;
 
-struct range_desc waiting_reads;
-struct range_desc waiting_writes;
-struct range_desc assigned_reads;
-struct range_desc assigned_writes;
-
-void init_range_desc(struct range_desc *head){
-	head->degree = -1;
-	head->range = -1;
-	head->tid = -1;
-	head->task = NULL;
-	head->type = -1;
-	head->assigned = -1;
-	INIT_LIST_HEAD(&head->node);
-}
-
-void init_range_lists(){
-	init_range_desc(&waiting_reads);
-	init_range_desc(&waiting_writes);
- 	init_range_desc(&assigned_reads);
-	init_range_desc(&assigned_writes);
-}
+struct range_desc waiting_reads = RANGE_DESC_INIT(waiting_reads);
+struct range_desc waiting_writes = RANGE_DESC_INIT(waiting_writes);
+struct range_desc assigned_reads = RANGE_DESC_INIT(assigned_reads);
+struct range_desc assigned_writes = RANGE_DESC_INIT(assigned_writes);
 
 /* common functions used by syscalls */
-static inline int check_param(int degree, int range);
+static inline bool range_valid(int range);
+static inline bool degree_valid(int degree);
 
-int A() /* always use with lock x */
+int reassign_rotlock() /* always use with lock x */
 {
 	/* declare variable */
 	int result = 0;
-  	/* check if there is a locked write lock
+	/* check if there is a locked write lock
 	 * if there is already a writer lock holding the rotation
 	 * no more lock can be allocated: return 0 */
 	struct range_desc *pos = NULL;
 	list_for_each_entry(pos, &assigned_writes.node, node){
 		if(range_in_rotation(pos))
-    		return 0;
-  	}
+			return 0;
+	}
 	/* check if there is a write lock waiting to be allocated.
 	 * If there is, check if there is an allocated lock that overlaps with it.
 	 * If there is no overlapping allocated lock, assign that waiting write lock. */
@@ -104,13 +88,14 @@ int A() /* always use with lock x */
 	return result;
 }
 
-int do_rotlock(int degree, int range, char lock_flag, struct range_desc *head)
+int do_rotlock(int degree, int range, enum rw_flag flag, struct range_desc *head)
 {
-	if(!check_param(degree, range))
+	if(!degree_valid(degree) || !range_valid(range))
 		return -EINVAL;
-	
+
 	struct range_desc* newitem =
 		(struct range_desc*) kmalloc(sizeof(struct range_desc), GFP_KERNEL);
+
 	if(newitem == NULL) {
 		printk("DEBUG: kernel has no memory\n");
 		return -ENOMEM;
@@ -120,18 +105,19 @@ int do_rotlock(int degree, int range, char lock_flag, struct range_desc *head)
 	newitem->range = range;
 	newitem->tid = task_pid_vnr(current);
 	newitem->task = current;
-	newitem->type = lock_flag;
+	newitem->type = flag;
 	newitem->assigned = 0;
 	INIT_LIST_HEAD(&newitem->node);
 
-	// TODO: grab lock x 
-	list_add_tail(&newitem->node, &head->node);	
+	// TODO: grab lock x
+	list_add_tail(&newitem->node, &head->node);
+
 	if(range_in_rotation(newitem))
-		A();
+		reassign_rotlock();
+	// TODO: release lock x
 
 	/* this sleep implementation prevents concurrency issues
 	 * see: http://www.linuxjournal.com/article/8144 */
-	// TODO: release lock x
 	set_current_state(TASK_INTERRUPTIBLE);
 	while(!newitem->assigned) {
 		schedule();
@@ -140,50 +126,53 @@ int do_rotlock(int degree, int range, char lock_flag, struct range_desc *head)
 	__set_current_state(TASK_RUNNING);
 
 	return 0;
-	
+
 }
 
-int do_rotunlock(int degree, int range, char lock_flag, struct range_desc* head)
+int do_rotunlock(int degree, int range, enum rw_flag flag, struct range_desc* head)
 {
-	if(!check_param(degree, range))
+	if(!degree_valid(degree) || !range_valid(range))
 		return -EINVAL;
-	
+
 	/* find that exact lock */
 	pid_t tid = task_pid_vnr(current);
 	struct range_desc *curr;
+
+	// TODO: set lock x
 	list_for_each_entry(curr, &head->node, node) {
-		if((curr->degree == degree)&&(curr->range == range)&&(curr->tid == tid))
+		if((curr->degree == degree)&&(curr->range == range)&&(curr->type == flag)&&(curr->tid == tid))
 			break;
 	}
+
 	if(curr == head) {	/* this is true only if iteration is finished */
 		printk("DEBUG: error: no match for rotunlock_read\n");
+		// TODO: unset lock x
 		return -EINVAL;
 	}
-	// todo: error check
-	
-	/* delete it from list.
-	 * If device lock is in curr's range, call A */
-	// todo: set lock x 
+
+	/* delete it from list. */
 	list_del(&curr->node);
 	kfree(curr);
-	if(range_in_rotation(curr))
-		A();	
-	// todo: unset lock x
+
+	reassign_rotlock();
+	// TODO: unset lock x
 
 	return 0;
 
 }
 
 int do_set_rotation(int degree)
-{	
-	if(degree < 0 || degree >= 360)
+{
+	int result;
+
+	if(!degree_valid(degree))
 		return -EINVAL;
-	/* update device rotation info */
+
+	// TODO: set lock x
 	device_rot = degree;
-	/* call A */
-	// todo: set lock x
-	int result = A();
-	// todo: unset lock x
+
+	result = reassign_rotlock();
+	// TODO: unset lock x
 	return result;
 }
 
@@ -195,36 +184,30 @@ SYSCALL_DEFINE1(set_rotation, int, degree)
 
 SYSCALL_DEFINE2(rotlock_read, int, degree, int, range)
 {
-	return do_rotlock(degree, range, READ_LOCK_FLAG, &waiting_reads);
+	return do_rotlock(degree, range, READ_FLAG, &waiting_reads);
 }
 
 SYSCALL_DEFINE2(rotlock_write, int, degree, int, range)
 {
-	return do_rotlock(degree, range, WRITE_LOCK_FLAG, &waiting_writes);
+	return do_rotlock(degree, range, WRITE_FLAG, &waiting_writes);
 }
 
 SYSCALL_DEFINE2(rotunlock_read, int, degree, int, range)
 {
-	return do_rotunlock(degree, range, READ_LOCK_FLAG, &assigned_reads);
+	return do_rotunlock(degree, range, READ_FLAG, &assigned_reads);
 }
 
 SYSCALL_DEFINE2(rotunlock_write, int, degree, int, range)
 {
-	return do_rotunlock(degree, range, WRITE_LOCK_FLAG, &assigned_writes);
+	return do_rotunlock(degree, range, WRITE_FLAG, &assigned_writes);
 }
 
-/* implementation of common functions */
-static inline int check_param(int degree, int range)
+static inline bool range_valid(int range)
 {
-	/* check if degree and range is correct */
-	if(degree < 0 || degree >= 360){
-		printk("DEBUG: error: degree out of range\n");
-		return 0;
-	}
-	if(range <= 0 || range >= 180){
-		printk("DEBUG: error: range out of range\n");
-		return 0;
-	}
-	return 1;
+	return 0 < range && range < 180;
 }
 
+static inline bool degree_valid(int degree)
+{
+	return 0 <= degree && degree < 360;
+}
