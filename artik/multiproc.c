f8ab6f4daa2812684ab73ca1564374fc516a9f92
Jhuni0123 <whdgnsdl887@gmail.com>
2017-05-11 03:13:57 +0900

    Fix and modify fork test code

--- artik/multiproc.c
+++ artik/multiproc.c
@@ -3,27 +3,39 @@
 #include <stdlib.h>
 #include <sched.h>
 #include <sys/types.h>
+#include <errno.h>
 
 
 int main()
 {
-	pid_t pid = getpid();
+	int pid = getpid();
 	int nProc = 10;
 	int nSleep = 1E9;
 	int i = 1;
+	struct sched_param param;
+	param.sched_priority = 0;
 
 	printf("Setting scheduler to WRR..\n");
-	if(sched_setscheduler(pid, 6, NULL) < 0)
+	if(sched_setscheduler(pid, 6, &param) < 0)
 	{
+		printf("ERRORNO %s\n", strerror(errno));
 		printf("sched_setscheduler failed.\n");
-		return;
+		return 1;
 	}
-	printf("%dth process created. policy: %d\n", i, sched_getscheduler(getpid()));
+	printf("%dth process %d created. policy: %d\n", i, getpid(), sched_getscheduler(pid));
 
 	while(fork() == 0)
 	{
+		pid = getpid();
 		i++;
-		printf("%dth process created. policy: %d\n", i, sched_getscheduler(getpid()));
+		
+		if(sched_setscheduler(pid, 6, &param) < 0)
+		{
+			printf("ERRORNO %s\n", strerror(errno));
+			printf("sched_setscheduler failed.\n");
+			return 1;
+		}
+		printf("%dth process %d created. policy: %d\n", i, pid, sched_getscheduler(getpid()));
 		if(i == nProc)
 			break;
 	}
@@ -32,8 +44,8 @@ int main()
 	while(cnt < nSleep)
 	{
 		cnt++;
-		if(cnt % 1E8 == 0)
-			printf("%dth process woken: %d\n", i, cnt / 1E8);
+		if(cnt % 100000000 == 0)
+			printf("%dth process %d woken: %d\n", i, pid, cnt / 100000000);
 	}
 
 }
