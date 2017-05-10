4e618ced8860c36fb8ab68228989bdce147878cb
dnsdhrj <dnsdhrj123@gmail.com>
2017-05-10 22:37:58 +0900

    Make multiprocess testing code

---
+++ artik/multiproc.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <sys/types.h>
+
+
+int main()
+{
+	pid_t pid = getpid();
+	int nProc = 10;
+	int nSleep = 10;
+	int i = 1;
+
+	printf("Setting scheduler to WRR..\n");
+	if(sched_setscheduler(pid, 6, NULL) < 0)
+	{
+		printf("sched_setscheduler failed.\n");
+		return;
+	}
+	printf("%dth process created. policy: %d\n", i, sched_getscheduler(getpid()));
+
+	while(fork() == 0)
+	{
+		i++;
+		printf("%dth process created. policy: %d\n", i, sched_getscheduler(getpid()));
+		if(i == nProc)
+			break;
+	}
+	
+	int cnt = 0;
+	while(cnt < nSleep)
+	{
+		cnt++;
+		printf("%dth process woken: %d\n", i, cnt);
+		sleep(1);
+	}
+
+}
